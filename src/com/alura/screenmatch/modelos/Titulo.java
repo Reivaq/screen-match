7a3d55a1f3be3edc680decebb76351f0c29999ef
Antonio Quiroz <antoniojavierquiroz@gmail.com>
2025-05-23 02:28:43 -0600

    Estructura de datos forma 1

--- src/com/alura/screenmatch/modelos/Titulo.java
+++ src/com/alura/screenmatch/modelos/Titulo.java
@@ -1,11 +1,16 @@
 package com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title") // esto se agrega para que el gson sepa a que nos est5amos refiriendo por que en nuestra gson vien year y title y no nombre y feca de lanzamiento
     private String nombre;
+    @SerializedName("Year")
     private int fechaDeLanzamiento;
     private boolean incluidoEnElPlan;
     private double sumaDeLasEvaluaciones;
     private int totalDeEvaluaciones;
+    @SerializedName("RunTime")
     private int duracionEnMinutos;
 
 
@@ -67,5 +72,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return
+                "nombre='" + nombre + '\'' +
+                ", fechaDeLanzamiento=" + fechaDeLanzamiento ;
     }
 }
--- src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,6 +1,9 @@
 package com.alura.screenmatch.principal;
 
 
+import com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -30,6 +33,18 @@ public class PrincipalConBusqueda {
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        // CONVERTIR GSON A NUESTRA CLASE
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo miTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(miTitulo);
+
+
+
+
     }
 }
